24e4d0107209d1ea95fc4a51d319c27ee5075588
Trebornide <robert.edin@gmail.com>
2017-03-01 21:14:38 +0100

    JSON-schema för hela modellen gick igenom.

--- test_schema.py
+++ test_schema.py
@@ -1,7 +1,12 @@
 from tsg import *
+from test import Conf
 
-class Conf(Configuration):
-    myAtom = T_ATOM(a='b')
+class SubConf(Section):
+    s1 = T_ATOM()
+
+#class Conf(Configuration):
+#    myAtom = T_ATOM(title='MyTitle', format='T_ATOM', layoutHint=('lowercase', ('vertical', 7)))
+#    sub = SubConf()
 
 conf = Conf()
 
